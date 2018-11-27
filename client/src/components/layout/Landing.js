36f401df10c46422daed51d46d2973d42c09269d
HanRui <597589373@qq.com>
2018-11-27 16:14:12 +0800

    避免登陆后显示landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-export default class Landing extends Component {
+
+import { connect } from "react-redux"
+import { PropTypes } from 'prop-types';
+
+class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard')
+    }
+  }
+
   render() {
     return (
       <div className="landing">
@@ -23,3 +33,14 @@ export default class Landing extends Component {
     )
   }
 }
+
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+}
+
+// 将状态映射为属性
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps)(Landing)
